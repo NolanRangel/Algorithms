75948bc775e274d8834da47859754c2420e7ddf3
NolanRangel <90576911+NolanRangel@users.noreply.github.com>
2022-01-24 13:46:12 -0700

    daily algo

---
+++ mern/algorithms/radixSort_ unsortedMedian.js
@@ -0,0 +1,24 @@
+//reference:
+//https://www.codingeek.com/wp-content/uploads/2017/02/radix.png
+
+// Loop through the array by each index
+// spread apart each sub index arr[i].map()? over each sub index.length
+// loop through the each sub unit and using a sort method sort
+
+
+const numsRandomOrder = [121, 432, 564, 23, 1, 45, 788];
+
+
+function radixSort(nums) {
+  //given an unsorted array, sort the array using radixSort
+  // sort all the numbers by the first digit. Then the 10digit etc..
+}
+
+
+/*
+Create a function that determines the median of an array of unsorted values. When Sosho claimed he could ‘name that tune’ in just O(N), the Dojo was all aflutter. Any solution is fine, but doing this in O(N) has been agreed-upon across the industry as an ‘interesting problem’.
+*/
+function unsortedMedian(nums){
+
+}
+
