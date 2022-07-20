1d4b89943527cd8184c98bd0d7517271418a0177
NolanRangel <90576911+NolanRangel@users.noreply.github.com>
2022-07-20 10:10:31 -0600

    hacker rank - marsExploration - js / py

---
+++ hacker_rank/week_2/marsExploration.js
@@ -0,0 +1,23 @@
+
+
+const marsExploration = (s) => {
+    // Write your code here
+    let count = 0;
+
+    for (let i = 0; i < s.length; i += 3) {
+        if (s[i] !== 'S') {
+            count++;
+        }
+        if (s[i + 1] !== 'O') {
+            count++;
+        }
+        if (s[i + 2] !== 'S') {
+            count++;
+        }
+    }
+    return count
+
+}
+
+console.log(marsExploration('SOSSPSSQSSOR'));
+console.log(marsExploration('SOSOOSOSOSOSOSSOSOSOSOSOSOS'));
